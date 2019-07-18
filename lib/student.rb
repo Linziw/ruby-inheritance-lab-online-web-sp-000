fb98032d6374b54419aee11c12f9cd62522b5a61
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-18 13:02:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,16 @@
+require "pry"
+
 class Student < User
+  
+  @@all = []
+  
+  attr_reader :all
   
    def initialize
     @first_name = first_name
     @last_name = last_name
     @knowledge = []
+    @@all << self
   end
   
   def learn(string)
@@ -13,5 +20,16 @@ class Student < User
   def knowledge
     @knowledge
   end
+  
+  def self.all
+  @@all 
+end
+
+def display_knowledge
+self.all.each do |student|
+ 
+puts "#{student.name} - #{student.knowledge}"
+end
+end
 
 end
